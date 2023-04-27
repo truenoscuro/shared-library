f878c0b61d256773ccc9ba71529abb3bccd0321d
oruiz <oruiz@3digits.es>
2023-04-27 17:06:18 +0200

    prueba bien

--- src/es/tresdigits/Step.groovy
+++ src/es/tresdigits/Step.groovy
@@ -4,6 +4,6 @@ class Step implements Serializable {
   def steps
   Step(steps) {this.steps = steps}
   def mvn(args) {
-      steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+      echo "${args}"
     }
 }
