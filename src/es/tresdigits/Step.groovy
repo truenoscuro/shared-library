ff573b5cb6ea9c955563674f3f8c0fbf6de4a88d
oruiz <oruiz@3digits.es>
2023-04-27 17:05:00 +0200

    step4

--- src/es/tresdigits/Step.groovy
+++ src/es/tresdigits/Step.groovy
@@ -2,7 +2,7 @@ package es.tresdigits
 
 class Step implements Serializable {
   def steps
-  Utilities(steps) {this.steps = steps}
+  Step(steps) {this.steps = steps}
   def mvn(args) {
       steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
     }
