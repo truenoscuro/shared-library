2f2626a2621a8daf5912e41a6d1810e56cd41e18
oruiz <oruiz@3digits.es>
2023-04-28 08:46:59 +0200

    step prova

--- src/es/tresdigits/Step.groovy
+++ src/es/tresdigits/Step.groovy
@@ -1,7 +1,17 @@
 package es.tresdigits
 
-class Step  {
-  static def mvn(steps,args) {  steps.echo "${args}"
-    }
-  
+class Step  implements Serializable {
+
+  def script
+  def env
+
+  def init(script, env){
+    this.script = script
+    this.env = env
+  }
+
+  def echo(args) {  script.echo "${args}" }
+  def holaMundo(){ script.echo "hola mundo"; }
+
+
 }
--- vars/log.groovy
+++ vars/log.groovy
@@ -1,6 +1,10 @@
 @groovy.transform.Field
 es.tresdigits.Step step = new es.tresdigits.Step()
 
+def init(sript, env){
+    step.init(sript,env)
+    return step;
+}
 
 def info(message) {
     echo "INFO: ${message}"
