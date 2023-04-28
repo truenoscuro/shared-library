165ab94e12963a1091baa25cc6f73ac7671d94f1
oruiz <oruiz@3digits.es>
2023-04-28 09:42:19 +0200

    cambio a td tres digits

--- vars/td.groovy
+++ vars/td.groovy
@@ -6,7 +6,13 @@ import es.tresdigits.jenkins.Utils
 
 // Field de clases
 @Field Utils utils = new Utils()
+
+
+
 // inicialitzacio de utils
 def init(script){
     utils.init(script)
 }
+
+
+// funcions de templades
