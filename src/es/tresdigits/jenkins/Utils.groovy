51c44de375fadaa5a29a8e7b1c8f9108d86b88c1
oruiz <oruiz@3digits.es>
2023-04-28 09:54:55 +0200

    enviaroment

--- src/es/tresdigits/jenkins/Utils.groovy
+++ src/es/tresdigits/jenkins/Utils.groovy
@@ -3,10 +3,14 @@ package es.tresdigits.jenkins
 class Utils  implements Serializable {
 
     def step
+    def env
 
-    def init(step){
+    def init(step,env){
         step.echo "Init Utils ..."
         this.step =step
+        this.env = env
+
+        step.echo "Enviaroment: ${env}"
 
     }
 
