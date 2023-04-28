2a281c0d46f7d3cd729551a72fd5810bb76d716f
oruiz <oruiz@3digits.es>
2023-04-28 11:00:16 +0200

    prueba 2

--- src/es/tresdigits/jenkins/Angular.groovy
+++ src/es/tresdigits/jenkins/Angular.groovy
@@ -17,7 +17,7 @@ class Angular  implements Serializable {
 
     def newProject(nameProject){
         step.echo "New Project..."
-        step.sh "ng new ${nameProject}"
-        step.sh "cd ${nameProject}"
+        step.sh ng new nameProject
+        step.sh cd ${nameProject}
     }
 }
