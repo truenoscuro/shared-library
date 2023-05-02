4619fc00908189866a20377ff39408ad2172bab2
oruiz <oruiz@3digits.es>
2023-05-02 12:59:57 +0200

    a ver

--- vars/td.groovy
+++ vars/td.groovy
@@ -70,18 +70,16 @@ def testAngular2(step,env,docker){
     //esto furula
     //Test
     String workSpace = utils.env['WORKSPACE'];
-    /*
     node.inside("-u root -v prod:'${workSpace}'/dist"){
         
         angular.install()
         utils.git()
         angular.iPackage()
         angular.build()
-        step.sh "pwd"
     }
-    */
+ 
     //apache RUn
-    def apacheContainer = apache.run("-p 80:80 -v prod:'${workSpace}'/var/www/html/.") 
+    //def apacheContainer = apache.run("-p 80:80 -v prod:'${workSpace}'/var/www/html/.") 
     
 }
 
