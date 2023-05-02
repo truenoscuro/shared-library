a08e842c5ee5dc850148596a5475c5551e107b0a
oruiz <oruiz@3digits.es>
2023-05-02 12:31:31 +0200

    a ve

--- vars/td.groovy
+++ vars/td.groovy
@@ -64,11 +64,11 @@ def testAngular2(step,env,docker){
     def apache = dk.image("php:7.2-apache")
 
     //apache RUn
-    //def apacheContainer = apache.run("-p 80:80 -v prod:/var/www/html") // base de dades fer-la correr
+    //def apacheContainer = apache.run("-p 80:80 ") // base de dades fer-la correr
     //esto furula
     //Test
 
-    node.inside("-u root -v prod:/${utils.env['WORKSPACE']}/dist/."){
+    node.inside("-u root -v prod:/${utils.env['WORKSPACE']}"){
         
         angular.install()
         utils.git()
