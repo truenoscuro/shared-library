572da6867f0040219112edb20efb75ce74d7e139
oruiz <oruiz@3digits.es>
2023-05-02 11:58:58 +0200

    s

--- vars/td.groovy
+++ vars/td.groovy
@@ -62,7 +62,7 @@ def testAngular2(step,env,docker){
 
     def node = dk.image("node")
     
-    def apache = dk.image("php:latest-apache")
+    def apache = dk.image("php:7.2-apache")
 
     //apache RUn
     apache.withRun("-u root -p 80:80 -w /var/www/html"){
