77115c528ba97b4deafbd3535d1af22114f4a919
oruiz <oruiz@3digits.es>
2023-05-03 09:21:23 +0200

    a

--- vars/td.groovy
+++ vars/td.groovy
@@ -98,7 +98,7 @@ def testDockerFile(script,env,docker){
     dk.init(script,docker,utils)
     script.echo "Test DockerFile"
     dk.generate("node")
-    script.sh "ls"
+    script.sh "ls ${script.env}"
 
 }
 
