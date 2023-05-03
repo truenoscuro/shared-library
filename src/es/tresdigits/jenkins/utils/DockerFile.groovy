0d9349269478ab08accd3bbcd94766c8ebff13cb
oruiz <oruiz@3digits.es>
2023-05-03 09:19:02 +0200

    a ver

--- src/es/tresdigits/jenkins/utils/DockerFile.groovy
+++ src/es/tresdigits/jenkins/utils/DockerFile.groovy
@@ -1,11 +1,11 @@
 package es.tresdigits.jenkins.utils
-
+import java.io.File 
 class DockerFile {
 
 
     static void generate( String workspace, String image ){
 
-        File dockerFile = new File("Dockerfile.${image}")
+        File dockerFile = new File("workspace/Dockerfile")
         dockerFile.createNewFile()
         /*
         String content=
