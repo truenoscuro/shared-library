f8bc8163929390cde4505f46e96c5c8d334824a4
oruiz <oruiz@3digits.es>
2023-05-03 11:30:49 +0200

    s

--- src/es/tresdigits/jenkins/utils/DockerFile.groovy
+++ src/es/tresdigits/jenkins/utils/DockerFile.groovy
@@ -17,8 +17,8 @@ class DockerFile {
     
     static String contentAngular(String gitUrl , String tagNode,String tagApache){
        
-        String directory = (gitUrl =~ /\/(\w|-|\d)+\.(?=(git))/)
-        directory = (directory =~ /(\w|-|\d)+/)
+        String directory = (gitUrl =~ /\/(\w|-|\d)+\.(?=(git))/).getAt(0)
+        directory = (directory =~ /(\w|-|\d)+/).getAt(0)
         String directoryBuilder = "/node/${directory}/dist/."
         String content= 
         """
