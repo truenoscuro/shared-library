de70fa86d26193f56c66108b2d4f58984e042d02
oruiz <oruiz@3digits.es>
2023-05-05 13:30:39 +0200

    da

--- src/es/tresdigits/jenkins/Sonar.groovy
+++ src/es/tresdigits/jenkins/Sonar.groovy
@@ -21,7 +21,7 @@ class Sonar  implements Serializable {
    
 
     def scannerBinaries = {
-        script.withSonarQubeEnv("${name}"){
+        withSonarQubeEnv("${name}"){
            script.sh "${scannerTool}/bin/sonar-scanner -Dproject.settings=sonar-project.properties -Dsonar.java.binaries=."
         }
     }
