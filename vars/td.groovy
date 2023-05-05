0463435d38fc215232fed79a52ce3d3e8bec2acf
oruiz <oruiz@3digits.es>
2023-05-05 11:17:04 +0200

    rew

--- vars/td.groovy
+++ vars/td.groovy
@@ -46,7 +46,7 @@ def git(String gitUrl="",String credentialsId=''){
     }   
 }
 // 
-def parallel(String[]... jobs){
+def parallel(String[String[script().error "Falta el url del git"]] jobs){
     // crees els jobs --> els he fas abaix
     
     script().parallel([
