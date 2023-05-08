58b28c7467fcf138e2a2817d20ea917b614e69f7
oruiz <oruiz@3digits.es>
2023-05-08 10:25:09 +0200

    432

--- vars/td.groovy
+++ vars/td.groovy
@@ -114,8 +114,13 @@ def parallel(Map jobs){
 //nombre obligatorio
 // convertir los arg en un mapa
 def dockerfileRun(String imageName="default ${utils.env.BUILD_NUMBER}",String dockerfile="Dockerfile", String path=".",args=""){
-    docker.remove()
-    docker.run(imageName,dockerfile,path,args)
+    
+    
+    script().stage("Build dockerfile"){
+        docker.remove()
+        docker.run(imageName,dockerfile,path,args)
+    }
+
 }
 
 
