d06f8c1459e7c24b4253cab4acd5e010bd9f61e7
oruiz <oruiz@3digits.es>
2023-05-09 11:59:03 +0200

    421

---
+++ src/es/tresdigits/jenkins/Ssh.groovy
@@ -0,0 +1,44 @@
+package es.tresdigits.jenkins
+
+
+class Ssh  implements Serializable {
+
+    def remote;
+    def script;
+    def utils;
+
+    def init(script,utils){
+        this.script = script
+        this.utils = utils
+    }
+
+
+    def addRemote(Map conf){
+
+        remote.name = (conf?.name != null )? conf.name : "server" 
+        remote.host = conf?.host 
+        remote.allowAnyHosts = true
+
+        if(cont?.credentialId != null){
+            def credential = script.withCredentials(
+                [sshUserPrivateKey(
+                    credentialsId: '${cont.credentialId}', 
+                    keyFileVariable: 'keyFile',
+                    passphraseVariable: '', 
+                    usernameVariable: 'userName'
+                )])
+            remote.user = credential.userName
+            remote.
+
+        }else{
+            remote.user = conf?.user
+            remote.password = conf?.password
+        }
+
+    }
+
+
+
+
+
+}
