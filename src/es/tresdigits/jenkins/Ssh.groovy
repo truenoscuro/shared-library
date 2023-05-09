0471ad1f41be4628dafcda683e46ee290b205adc
oruiz <oruiz@3digits.es>
2023-05-09 16:13:44 +0200

    reqr

--- src/es/tresdigits/jenkins/Ssh.groovy
+++ src/es/tresdigits/jenkins/Ssh.groovy
@@ -77,7 +77,7 @@ class Ssh  implements Serializable {
         String[] listFiles = utils.listFiles("target")
         String pack = "./target/${utils.findJarWar()}"
         String path ="./docker"
-        name = "dockerTest-${language}"
+        def name = "dockerTest-${language}"
         def sshCom = {
             com("rm -fr ${path}",isSudo) // eliminacion de carpeta
             com("mkdir ${path}")
