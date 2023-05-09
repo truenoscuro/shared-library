d82c1b1ad5cc270fbe41756f5114caa88fc1489e
oruiz <oruiz@3digits.es>
2023-05-09 10:06:49 +0200

    32

--- vars/td.groovy
+++ vars/td.groovy
@@ -33,7 +33,7 @@ def init(script,env,sonarName="${sonarName}",nameTool="${nameTool}"){
         docker.init(script,utils) 
         //Tod init de ses funcions!
         maven.init(script,utils)
-        sonar.init(script,utils,sonarName,nameTool)
+        //sonar.init(script,utils,sonarName,nameTool)
     }
 }
 //Stes i geters
