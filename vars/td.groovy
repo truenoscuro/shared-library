c4fe8f54e48fca4ebdb253b1fbf901ad2252fee8
oruiz <oruiz@3digits.es>
2023-05-09 16:09:46 +0200

    reqw

--- vars/td.groovy
+++ vars/td.groovy
@@ -132,7 +132,7 @@ def sshComand(Map conf,String command,boolean isSudo = false){
 }
 
 
-def sshRunDocker(String name="dockerTest",Map conf,String language,boolean isSudo = false){
+def sshRunDocker(Map conf,String language,String name="dockerTest",boolean isSudo = false){
     def script = script()
     script.stage(" docker run ssh"){
         ssh.docker(name,conf,language,isSudo)
