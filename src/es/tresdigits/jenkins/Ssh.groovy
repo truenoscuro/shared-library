45bcca62e0d10d707d5ff79c7cca0b3761894377
oruiz <oruiz@3digits.es>
2023-05-09 12:41:17 +0200

    reqw

--- src/es/tresdigits/jenkins/Ssh.groovy
+++ src/es/tresdigits/jenkins/Ssh.groovy
@@ -28,22 +28,38 @@ class Ssh  implements Serializable {
 
     }
 
+    def credentials(Closure body){
+         script.withCredentials([script.sshUserPrivateKey(
+                credentialsId: "${credentialsId}", 
+                keyFileVariable: 'keyFile',
+                passphraseVariable: '', 
+                usernameVariable: 'userName')]) {
+                body()
+        }
+
+
+    }
+
     def command(Map conf, String command,boolean isSudo){
+        
+        //TODO te configuracion --> no afegir
         addRemote(conf)
         
         if(credentialsId == null){
             script.sshCommand remote: this.remote, command: command
         }else{
-            script.withCredentials([script.sshUserPrivateKey(
-                credentialsId: "${credentialsId}", 
-                keyFileVariable: 'keyFile',
-                passphraseVariable: '', 
-                usernameVariable: 'userName')]) {
+            credentials({
             remote.user =  script.userName
             remote.identityFile = script.keyFile 
             script.sshCommand remote: remote , command: "${command}", sudo: isSudo
-            }
+            })
         }
+    }
+
+
+    def docker(Map conf,String compiler){
+
+
     }
 
 
