63b01bf5a21a9c63b0c7a1d335fb98d8a1dbbc36
oruiz <oruiz@3digits.es>
2023-05-10 11:35:33 +0200

    rewq

--- vars/td.groovy
+++ vars/td.groovy
@@ -46,6 +46,18 @@ def stage = { Map jobs ->
     
 }
 
+@Field 
+def parallel = { Map jobs -> 
+    def stages = [:]
+    jobs.each{
+        key,funct ->
+            String name = "${key}"
+            if(funct instanceof String ) name = "${name}-${funct}"
+            //stages["${name}"] = switchFunction(key,funct)
+    }
+    script().parallel(stages)
+    
+}
 
 
 
