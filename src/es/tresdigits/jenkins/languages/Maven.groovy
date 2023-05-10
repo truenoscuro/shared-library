626962684d1fc10edff551fbbcb37936653a28ff
oruiz <oruiz@3digits.es>
2023-05-10 15:31:26 +0200

    rewq

--- src/es/tresdigits/jenkins/languages/Maven.groovy
+++ src/es/tresdigits/jenkins/languages/Maven.groovy
@@ -4,31 +4,13 @@ package es.tresdigits.jenkins.languages
 class Maven {
 
     def utils
-    def conf
 
-    Maven( utils ,conf){
-        this.utils = utils 
-        this.conf = conf 
-    }
+    Maven( utils ){  this.utils = utils  }
     
-      
-    
-    def mvnV = {
-        utils.script.withMaven(){
-            utils.cmd "mvn -v"
-        }
-    } 
-       
-    
-
-
-    /*
-    //def mvnV = withMaven(utils.cmd "mvn -v")
-    def pack =  withMaven(  { utils.cmd "mvn clean package" } )
-        
+    def pack = { utils.cmd "mvn clean package"}
     def install = { utils.cmd "mvn clean install" }
     def compile = { utils.cmd "mvn clean compile" }
-    */    
+    
 
 
 
