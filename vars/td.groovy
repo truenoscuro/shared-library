e10d0a5b65193ebcc4bc3e8ce9aa1c11eb6d84b8
oruiz <oruiz@3digits.es>
2023-05-10 11:41:54 +0200

    r

--- vars/td.groovy
+++ vars/td.groovy
@@ -20,7 +20,7 @@ def init( script , env , globalConfig ){
 //nom a al stage
 
 @Field
-def git = { Map conf = utils.globalConfig.git ) ->
+def git = { Map conf = utils.globalConfig.git  ->
     def script = utils.script
     def git = utils.git
     script.stage("git clone de ${conf.url}"){
