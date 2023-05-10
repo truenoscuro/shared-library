128f39a20f5c6f5494c5d27a10d59cff900c6ef9
oruiz <oruiz@3digits.es>
2023-05-10 11:16:55 +0200

    reqwfds

--- vars/td.groovy
+++ vars/td.groovy
@@ -18,6 +18,7 @@ def init( script , env , globalConfig ){
     utils = new Utils(script , env , globalConfig )
 }
 //nom a al stage
+@Field
 def git ( Map conf = utils.globalConfig.git ){
     
     def script = utils.script
