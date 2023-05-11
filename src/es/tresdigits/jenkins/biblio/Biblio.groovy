3e30e64ebe45c0a376b36235a8ca63322b820a00
oruiz <oruiz@3digits.es>
2023-05-11 10:00:01 +0200

    reqw

---
+++ src/es/tresdigits/jenkins/biblio/Biblio.groovy
@@ -0,0 +1,63 @@
+package es.tresdigits.jenkins.biblio
+
+class Biblio{
+
+    private static languagues =[
+
+        maven:["maven","m","mvn","spring","springboot","boot"],                 //m
+        angular:["angular","a","ng","node","npm"],                              //a
+        sonar:["sonar","s","sonarq","scanner","sonar-scanner","sonarscaner"]    //s
+
+
+    ]
+
+    private static funct = [
+
+        build:["build","b","construir","constr"],                               //b
+        install:["install","i","instalar","inst"],                              //i
+        scanner:["scanner","s","scan","escaner"],                               //s
+        pack:["pack","p","package","empaquetar"],                               //p
+        compile:["compile","c","comp","compilar"],                              //c
+
+
+
+
+    ]
+
+
+    static String getLang(String lang){
+        String solv = ""
+        languages.each{ name,list ->
+            list.each{ alias ->
+                if(alias == lang) {
+                    solv = name
+                    break
+                }
+            }
+            if(solv != "") break
+        }
+        return solv
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/es/tresdigits/jenkins/languages/Angular.groovy
+++ src/es/tresdigits/jenkins/languages/Angular.groovy
@@ -8,14 +8,10 @@ class Angular  {
         this.utils =  utils  
     }
 
-    def npmV =  {  utils.cmd "npm -v" }
-    def nodeV =  {  utils.cmd "node -v" }
-    
     def iAngular = { utils.cmd "npm install -g @angular/cli"}
     
     //install package.json
     def install = {utils.cmd "npm i"}
-    def i = {utils.cmd "npm i"}
     //ng funct
     
     def build = { utils.cmd "ng build"}
