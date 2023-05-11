16898ad827f694ff0fc78774938674105fd18a49
oruiz <oruiz@3digits.es>
2023-05-11 12:07:15 +0200

    4321

--- vars/td.groovy
+++ vars/td.groovy
@@ -45,8 +45,11 @@ def parallel( Map jobs ){
             if(funct instanceof String ) name = "${name}-${funct}"
             stages["${name}"] =  switcher.getFunct(key,funct)
     }
+
+    script.stage("parallel stage"){
+        script.parallel(stages)
+    }
    
-    script.parallel(stages)
   
    
 }
