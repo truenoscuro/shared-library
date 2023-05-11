4698c0db169959a73d0b264c335ca3037f2b1f51
oruiz <oruiz@3digits.es>
2023-05-11 11:27:04 +0200

    rewq

--- src/es/tresdigits/jenkins/languages/Sonar.groovy
+++ src/es/tresdigits/jenkins/languages/Sonar.groovy
@@ -13,7 +13,7 @@ class Sonar  {
     Sonar( utils , conf ){ 
         this.utils = utils
         this.conf = [:]
-        this.conf["path"] = conf.path ?: "${utils.tool(conf.tool)}/bin/sonar-scanner"
+       
         this.conf["name"] = conf.name
         this.conf["properties"] = conf.properties
         if(conf.haveBinaries) this.conf["binaries"] = "-Dsonar.java.binaries=${conf.binaries}"
@@ -21,9 +21,10 @@ class Sonar  {
     
 
     def scanner  = {
+        this.conf["path"] = conf.path ?: "${utils.tool(conf.tool)}/bin/sonar-scanner"
         utils.script.withSonarQubeEnv("${conf.name}"){
            utils.cmd "${conf.path} -Dproject.settings=${conf.properties} ${conf.binaries}"
-        }.call()
+        }.call
     }
     
    
